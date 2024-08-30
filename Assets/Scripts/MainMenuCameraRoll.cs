4eef36a83d3075be3caff8b8dc1cea65bd25f8a8
Sluicee <sluicee1@gmail.com>
2024-08-30 17:32:16 +0400

    MainMenuCameraRoll.cs

---
+++ Assets/Scripts/MainMenuCameraRoll.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class MainMenuCameraRoll : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed = 10f; // Скорость вращения
+
+    void Update()
+    {
+        // Вращение камеры по оси X
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+    }
+}
